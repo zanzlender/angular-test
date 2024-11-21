b88ec32cb417d45b7601a8b21e6cb9360806b398
zanzlender <a1zan.zlender@gmail.com>
2024-11-21 18:48:40 +0100

    added tailwindcss and angular material

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [],
 };
