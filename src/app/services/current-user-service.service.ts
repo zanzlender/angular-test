106735f215d338f7e3d09bf16a7a9d57035d8501
zanzlender <a1zan.zlender@gmail.com>
2024-11-22 14:32:37 +0100

    added products and logged in user services

---
+++ src/app/services/current-user-service.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { LoggedInUser } from '../models/logged-in-user.model';
+import { LocalStorageService } from './local-storage.service';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CurrentUserServiceService {
+  constructor(private localStorageService: LocalStorageService) {}
+  currentUser$ = new BehaviorSubject<LoggedInUser | undefined>(undefined);
+
+  setCurrentUser() {
+    const productsString = this.localStorageService.getItem('auth_token');
+    if (!productsString) {
+      this.currentUser$.next(undefined);
+      return;
+    }
+    try {
+      const user = JSON.parse(productsString) as LoggedInUser;
+      this.currentUser$.next(user);
+    } catch (err) {
+      this.currentUser$.next(undefined);
+    }
+  }
+
+  signInUser({ username, password }: { username: string; password: string }) {
+    if (username === 'admin' && password === 'password') {
+      const newUser: LoggedInUser = {
+        id: '1',
+        name: 'admin',
+        token: 'valid-auth-token',
+      };
+      this.localStorageService.setItem('auth_token', JSON.stringify(newUser));
+
+      this.currentUser$.next(newUser);
+    }
+  }
+}
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,108 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { LocalStorageService } from './local-storage.service';
+import { Product, ProductInsert } from '../models/product.model';
+
+const LOCAL_STORAGE_PRODUCTS_KEY = 'products';
+
+@Injectable({
+  providedIn: 'root',
+})
+// TODO: switch to w/e fetching method
+export class ProductsService {
+  constructor(
+    private http: HttpClient,
+    private localStorageService: LocalStorageService
+  ) {}
+
+  getProducts(): Observable<Product[]> {
+    const fetchProducts = new Promise<Product[]>((resolve, reject) => {
+      try {
+        const products = this.localStorageService.getItem(
+          LOCAL_STORAGE_PRODUCTS_KEY
+        );
+        const productsData = (
+          products ? JSON.parse(products) : []
+        ) as Product[];
+        resolve(productsData);
+      } catch (err) {
+        reject(err);
+      }
+    });
+
+    const productsObservable = from(fetchProducts);
+    return productsObservable;
+  }
+
+  createProduct(product: ProductInsert): Observable<Product> {
+    const productsString = this.localStorageService.getItem(
+      LOCAL_STORAGE_PRODUCTS_KEY
+    );
+    const products = (
+      productsString ? JSON.parse(productsString) : []
+    ) as Product[];
+
+    const newId =
+      products.reduce((prev, val) => {
+        if (val.id > prev) {
+          return val.id;
+        }
+        return prev;
+      }, 1) + 1;
+
+    products.push({
+      ...product,
+      id: newId,
+    });
+
+    this.localStorageService.setItem(
+      LOCAL_STORAGE_PRODUCTS_KEY,
+      JSON.stringify(products)
+    );
+
+    return new Observable<Product>((observer) => {
+      observer.next({
+        ...product,
+        id: newId,
+      });
+    });
+  }
+
+  updateProduct(product: Product): void {
+    const productsString = this.localStorageService.getItem(
+      LOCAL_STORAGE_PRODUCTS_KEY
+    );
+    const products = (
+      productsString ? JSON.parse(productsString) : []
+    ) as Product[];
+
+    const index = products.findIndex((p) => p.id === product.id);
+    if (index === -1) return;
+
+    products[index] = product;
+
+    this.localStorageService.setItem(
+      LOCAL_STORAGE_PRODUCTS_KEY,
+      JSON.stringify(products)
+    );
+  }
+
+  deleteProduct(productId: number): void {
+    const productsString = this.localStorageService.getItem(
+      LOCAL_STORAGE_PRODUCTS_KEY
+    );
+    const products = (
+      productsString ? JSON.parse(productsString) : []
+    ) as Product[];
+
+    const updatedProducts = products.filter(
+      (product) => product.id !== productId
+    );
+
+    this.localStorageService.setItem(
+      LOCAL_STORAGE_PRODUCTS_KEY,
+      JSON.stringify(updatedProducts)
+    );
+  }
+}
